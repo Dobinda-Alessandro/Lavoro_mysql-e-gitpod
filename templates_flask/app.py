59e93a779950000399a1485f5ce8bbdba3240d9e
Dobinda-Alessandro <dobinda.alessandro@iisgalvanimi.edu.it>
2023-04-13 11:01:24 +0000

    3

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -1,10 +1,25 @@
 from flask import render_template
 from flask import Flask
-
+import mysql.connector              #devo connettere la tabella clash royale 
 
 app = Flask(__name__)
+
+#connettere a mysql
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+  database="CLASH_ROYALE"
+)
+mycursor = mydb.cursor()
 
 
 @app.route('/')
 def hello():
-    return render_template('hello.html', name='Fabio')
+    return render_template('hello.html', name='Alessandro')
+
+@app.route('/units')
+def unitList():
+    mycursor.execute("Select * FROM Clash_Unit")
+    myresult = mycursor.fetchall()
+    return render_template('Clash_units.html', units=myresult)
